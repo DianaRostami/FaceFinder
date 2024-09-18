a559805b1faa4836a3199d697cf25dc11451af1e
DianaRostami <diana84rostami@gmail.com>
2024-09-18 14:48:53 +0330

    Initial commit

---
+++ FaceFinder.py
@@ -0,0 +1,25 @@
+import cv2
+
+# Load cascade
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+
+# Check if the cascade is loaded successfully
+if face_cascade.empty():
+    print("Error: Cascade Classifier not loaded!")
+    exit(0)
+our_image_color = cv2.imread("Replace\\with\\the\\path\\to\\your\\image.")
+
+# Convert image to grayscale
+our_image_gray = cv2.cvtColor(our_image_color, cv2.COLOR_BGR2GRAY)
+
+# Detect faces
+faces = face_cascade.detectMultiScale(our_image_gray, scaleFactor=1.05, minNeighbors=5)
+
+# Draw a rectangle around each face
+for x, y, w, h in faces:
+    cv2.rectangle(our_image_color, (x, y), (x+w, y+h), (0, 255, 0), 3)
+
+# Show image
+cv2.imshow("Face Detection", our_image_color)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
